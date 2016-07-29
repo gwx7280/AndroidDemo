d629a6900991274b80858494d29d6f413e5e18fb
jimmypangpang <149945171@qq.com>
2016-07-29 17:43:55 +0800

    增加了一些butterknife的demo

--- app/src/main/java/jimmy/com/androiddemo/butterknife/ButterKnifeActivity.java
+++ app/src/main/java/jimmy/com/androiddemo/butterknife/ButterKnifeActivity.java
@@ -2,7 +2,14 @@ package jimmy.com.androiddemo.butterknife;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+import jimmy.com.androiddemo.R;
 import jimmy.com.androiddemo.view.base.BaseActivity;
 
 /**
@@ -11,10 +18,23 @@ import jimmy.com.androiddemo.view.base.BaseActivity;
 public class ButterKnifeActivity extends BaseActivity {
 
 
+    @BindView(value = R.id.butterknife_textview)
+    private TextView textview;
+
+    @BindView(value = R.id.butterknife_button)
+    private Button button;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.butterknife_activity);
     }
+
+    @OnClick()
+    public void onClick(View v){
+        Toast.makeText(this,"哈哈哈，成功了",Toast.LENGTH_LONG).show();
+    }
+
+
 }
