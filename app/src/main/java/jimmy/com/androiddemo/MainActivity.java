1e47e0e4d21a03ff3ed10931f42e8d904dfbcbef
jimmypangpang <149945171@qq.com>
2016-07-29 16:21:35 +0800

    添加ButterKnife在基类中的使用

--- app/src/main/java/jimmy/com/androiddemo/MainActivity.java
+++ app/src/main/java/jimmy/com/androiddemo/MainActivity.java
@@ -1,13 +1,21 @@
 package jimmy.com.androiddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import jimmy.com.androiddemo.view.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Bind(value = R.id.hello)
+    TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("tag",hello==null?"null":"successed");
     }
 }
